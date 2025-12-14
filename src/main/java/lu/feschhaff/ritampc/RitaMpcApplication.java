8667fecce0e11249651172b28048553c07dca12d
Henri-Welsch <weljo870@outlook.de>
2025-12-14 20:13:08 +0100

    [Improvement][MT-13] Initial Commit

---
+++ src/main/java/lu/feschhaff/ritampc/RitaMpcApplication.java
@@ -0,0 +1,12 @@
+package lu.feschhaff.ritampc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RitaMpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RitaMpcApplication.class, args);
+    }
+}
---
+++ src/test/java/lu/feschhaff/ritampc/RitaMpcApplicationTests.java
@@ -0,0 +1,13 @@
+package lu.feschhaff.ritampc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RitaMpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
