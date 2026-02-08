53dcc3149c26d0595ceebe00d3b1ee335aa367c4
Henri-Welsch <weljo870@outlook.de>
2026-02-08 17:05:05 +0100

    [Improvement] Improved BoosterRegistry to also handel meta-files and a new folder structure. The Registry can now also search for and find the best booster model based on a given target.

--- src/main/java/lu/feschhaff/ritampc/services/BoosterRegistry.java
+++ src/main/java/lu/feschhaff/ritampc/services/BoosterRegistry.java
@@ -4,12 +4,14 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.log4j.Log4j2;
 import lu.feschhaff.ritampc.models.objects.BoosterModel;
 import lu.feschhaff.ritampc.models.objects.BoosterSubset;
+import lu.feschhaff.ritampc.models.objects.GeneralMetaData;
+import lu.feschhaff.ritampc.models.objects.ModelMetaData;
 import ml.dmlc.xgboost4j.java.Booster;
 import ml.dmlc.xgboost4j.java.XGBoost;
 import ml.dmlc.xgboost4j.java.XGBoostError;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
-import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -17,6 +19,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
@@ -35,24 +38,18 @@ public class BoosterRegistry {
 
     private final Map<String, BoosterSubset> boosterRegistry = new ConcurrentHashMap<>();
     private final ObjectMapper objectMapper = new ObjectMapper();
-//    private final ConnectionHandler connectionHandler;
-//    private final ConfigurableApplicationContext configurableApplicationContext;
-//
-//    public BoosterRegistry(ConfigurableApplicationContext configurableApplicationContext) {
-//        this.configurableApplicationContext = configurableApplicationContext;
-//    }
+    private final StateStoreService stateStore;
+    private final ConfigurableApplicationContext configurableApplicationContext;
 
     @Value("${booster.model.location}")
     private String boosterModelRootPath = "C:/Users/WelJo/Desktop/testFolder";
 
-//    public BoosterRegistry(ConnectionHandler connectionHandler) {
-//        this.connectionHandler = connectionHandler;
-//    }
-
-    public static void main(String[] args) throws IOException {
-        BoosterRegistry boosterRegistry = new BoosterRegistry();
-
-        boosterRegistry.clearAndRegisterBoostersFromFolder();
+    public BoosterRegistry(
+            StateStoreService stateStore,
+            ConfigurableApplicationContext configurableApplicationContext
+    ) {
+        this.stateStore = stateStore;
+        this.configurableApplicationContext = configurableApplicationContext;
     }
 
     @PostConstruct
@@ -60,14 +57,13 @@ public class BoosterRegistry {
         String message = "Trying to initialize booster registry from [boosterModelLocation: {}]";
         log.info(message, boosterModelRootPath);
 
-//        try {
-//            clearAndRegisterBoostersFromFolder();
-//        } catch (IOException e) {
-//            log.error("Unable to initialize booster registry, shutting system down!", e);
-//            clearAndRegisterBoostersFromFolder.close();
-//        }
+        try {
+            clearAndRegisterBoostersFromFolder();
+        } catch (IOException e) {
+            log.error("Unable to initialize booster registry, shutting system down!", e);
+            configurableApplicationContext.close();
+        }
     }
-
 
     private void clearAndRegisterBoostersFromFolder() throws IOException {
         boosterRegistry.clear();
@@ -85,9 +81,10 @@ public class BoosterRegistry {
 
     private BoosterSubset loadBoosterSubset(Path targetFolder) throws IOException {
         Path metaPath = targetFolder.resolve("meta.json");
-        Set<String> allPossibleFeatures = objectMapper.readValue(metaPath, new TypeReference<>() {});
 
-        BoosterSubset boosterSubset = new BoosterSubset(allPossibleFeatures);
+        GeneralMetaData generalMetaData = objectMapper.readValue(metaPath, GeneralMetaData.class);
+
+        BoosterSubset boosterSubset = new BoosterSubset(generalMetaData);
         Set<Path> strategyFolders = getSubsetFolders(targetFolder.toString());
 
         for (Path strategyFolder : strategyFolders) {
@@ -95,7 +92,7 @@ public class BoosterRegistry {
 
             try {
                 BoosterModel boosterModel = loadBoosterModel(strategyFolder);
-                boosterSubset.getBoosterModel().add(boosterModel);
+                boosterSubset.getBoosterModels().add(boosterModel);
             } catch (XGBoostError e) {
                 log.error("Unable to load booster from folder: {}", strategyFolder, e);
             }
@@ -106,18 +103,31 @@ public class BoosterRegistry {
 
     private BoosterModel loadBoosterModel(Path boosterFolder) throws XGBoostError {
         Path boosterMetaPath = boosterFolder.resolve("meta.json");
-        Set<String> possibleFeatures = objectMapper.readValue(boosterMetaPath, new TypeReference<>() {});
+        ModelMetaData modelMetaData = objectMapper.readValue(boosterMetaPath, ModelMetaData.class);
 
-        Path boosterModelPath = boosterFolder.resolve("booster.json");
+        Path boosterModelPath = boosterFolder.resolve("model.json");
         Booster booster = XGBoost.loadModel(boosterModelPath.toString());
 
-        return new BoosterModel(possibleFeatures, booster);
+        return new BoosterModel(modelMetaData, booster);
     }
-
 
     public static Set<Path> getSubsetFolders(String dir) throws IOException {
         try (Stream<Path> stream = Files.list(Paths.get(dir))) {
             return stream.collect(Collectors.toSet());
         }
+    }
+
+    public Optional<BoosterModel> findBestBooster(String target) {
+        BoosterSubset boosterSubset = this.boosterRegistry.get(target);
+        Set<String> featuresToCheck = boosterSubset.getGeneralMetaData().getAvailableFeatures();
+        Set<String> availableFeatures = stateStore.getPresentFeatures(featuresToCheck);
+
+        return findBooster(target, availableFeatures);
+    }
+
+    private Optional<BoosterModel> findBooster(String target, Set<String> features) {
+        return this.boosterRegistry.get(target).getBoosterModels().stream()
+                .filter(boosterModel -> boosterModel.containsAll(features))
+                .findFirst();
     }
 }
